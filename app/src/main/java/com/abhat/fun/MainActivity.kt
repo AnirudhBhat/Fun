86b1c5d1d6b67f6cdd4b823f4681d4588bcdad90
Kamal Agrawal <kamal.a@cumulations.com>
2017-05-27 19:11:50 +0530

    added kotlin android extensions

--- app/src/main/java/com/abhat/fun/MainActivity.kt
+++ app/src/main/java/com/abhat/fun/MainActivity.kt
@@ -4,41 +4,36 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.SwitchCompat
 import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
 
-    private var mGood: SwitchCompat? = null
-    private var mCheap: SwitchCompat? = null
-    private var mFast: SwitchCompat? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mGood = findViewById(R.id.good) as SwitchCompat
-        mCheap = findViewById(R.id.cheap) as SwitchCompat
-        mFast = findViewById(R.id.fast) as SwitchCompat
 
-        mGood!!.setOnCheckedChangeListener(this)
-        mCheap!!.setOnCheckedChangeListener(this)
-        mFast!!.setOnCheckedChangeListener(this)
+        good.setOnCheckedChangeListener(this)
+        cheap.setOnCheckedChangeListener(this)
+        fast.setOnCheckedChangeListener(this)
     }
 
     override fun onCheckedChanged(compoundButton: CompoundButton, checked: Boolean) {
         when (compoundButton.id) {
-            R.id.good -> if (mCheap!!.isChecked && mFast!!.isChecked) {
-                mFast!!.setOnCheckedChangeListener(null)
-                mFast!!.isChecked = false
-                mFast!!.setOnCheckedChangeListener(this)
+            R.id.good -> if (cheap.isChecked && fast.isChecked) {
+                fast.setOnCheckedChangeListener(null)
+                fast.isChecked = false
+                fast.setOnCheckedChangeListener(this)
             }
-            R.id.cheap -> if (mGood!!.isChecked && mFast!!.isChecked) {
-                mGood!!.setOnCheckedChangeListener(null)
-                mGood!!.isChecked = false
-                mGood!!.setOnCheckedChangeListener(this)
+            R.id.cheap -> if (good.isChecked && fast.isChecked) {
+                good.setOnCheckedChangeListener(null)
+                good.isChecked = false
+                good.setOnCheckedChangeListener(this)
             }
-            R.id.fast -> if (mGood!!.isChecked && mCheap!!.isChecked) {
-                mCheap!!.setOnCheckedChangeListener(null)
-                mCheap!!.isChecked = false
-                mCheap!!.setOnCheckedChangeListener(this)
+            R.id.fast -> if (good.isChecked && cheap.isChecked) {
+                cheap.setOnCheckedChangeListener(null)
+                cheap.isChecked = false
+                cheap.setOnCheckedChangeListener(this)
             }
         }
     }
